74362d4a9a8e59438acfe6978b5db21ebddd551d
dannyh564388 <da170140@ucf.edu>
2025-07-23 19:17:07 -0400

    Update prediction.js - 2025-07-23 19:17:07

--- prediction.js
+++ prediction.js
@@ -1,6 +1,6 @@
 const predictionData = {
-    date: "2025-07-22",
-    prediction: "Gap Up",
-    numeric: 1,
-    generated_at: "2025-07-22 16:08:17"
+    date: "2025-07-23",
+    prediction: "No Gap",
+    numeric: 0,
+    generated_at: "2025-07-23 19:17:07"
 };
